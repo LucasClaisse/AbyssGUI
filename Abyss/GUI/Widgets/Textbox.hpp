9cd3619ad0b58ac15dbfca6265b161d1ec4dab3c
HakanaiRuby <hakanairuby@gmail.com>
2019-08-13 19:11:08 +0200

    Modified example to include Window Body

--- Abyss/GUI/Widgets/Textbox.hpp
+++ Abyss/GUI/Widgets/Textbox.hpp
@@ -5,30 +5,36 @@
 #include <iostream>
 
 namespace Abyss {
-	namespace GUI {
-		namespace Widgets {
-			class Textbox;
-		}
-	}
+    namespace GUI {
+        namespace Widgets {
+            class Textbox;
+        }
+    }
 }
 
 class Abyss::GUI::Widgets::Textbox : public Widget {
 protected:
-	std::wstring m_wstr{};
+    std::wstring m_wstr{};
 
 public:
-	virtual ~Textbox() = default;
+    virtual ~Textbox() = default;
 
     auto characterEvent(const InputDevice &device, wchar_t codepoint) -> void override;
-	auto paint(const InputDevice &device, double dt, Renderer &renderer) -> void override;
+    auto paint(const InputDevice &device, double dt, Renderer &renderer) -> void override;
 };
 
 inline auto Abyss::GUI::Widgets::Textbox::characterEvent(const InputDevice &device, wchar_t codepoint) -> void
 {
-	m_wstr += codepoint;
+    m_wstr += codepoint;
 }
 
 inline auto Abyss::GUI::Widgets::Textbox::paint(const InputDevice &device, double dt, Renderer &renderer) -> void
 {
-	std::wcout << m_wstr << '\n';
+    std::wcout << m_wstr << '\n';
+    auto pos{getPosition()};
+    auto size{getSize()};
+
+    renderer.setColor({150, 150, 255});
+    renderer.drawRectangle(pos, size);
+    // renderer.drawText(L"Window Test", pos + Vector3<double>{6, 3}, "OpenSans-Regular.ttf", 18, {50, 50, 50});
 }
--- Example/example.cpp
+++ Example/example.cpp
@@ -8,21 +8,21 @@
 #include <map>
 #include <sstream>
 #include <stdexcept>
+#include <thread>
 #include <unordered_map>
 
-class TabBar : public Abyss::GUI::Widgets::RelativeLayout {
+class WindowBody final : public Abyss::GUI::Widgets::RelativeLayout {
 public:
     auto paint(const Abyss::GUI::InputDevice &device, double dt, Abyss::GUI::Renderer &renderer) -> void final;
 };
 
-inline auto TabBar::paint(const Abyss::GUI::InputDevice &device, double, Abyss::GUI::Renderer &renderer) -> void
+inline auto WindowBody::paint(const Abyss::GUI::InputDevice &device, double, Abyss::GUI::Renderer &renderer) -> void
 {
     auto pos{getPosition()};
     auto size{getSize()};
 
-    renderer.setColor({255, 255, 0});
+    renderer.setColor({255, 255, 255});
     renderer.drawRectangle(pos, size);
-    renderer.drawText(L"Tab 1\tTab 2\tTab 3", pos + Vector3<double>{10, 3}, "OpenSans-Regular.ttf", size.y - 3 * 3, {205, 205, 205});
 }
 
 class SFMLWindow final : public Abyss::GUI::Widget {
@@ -30,6 +30,7 @@ private:
     bool                              m_held{false};
     Vector3<double>                   m_heldPosition{};
     std::unique_ptr<sf::RenderWindow> m_window{};
+    WindowBody *                      m_body{nullptr};
 
 public:
     explicit SFMLWindow();
@@ -38,6 +39,7 @@ public:
     auto               setPosition(const decltype(m_position) &position) -> void final;
     [[nodiscard]] auto getSize() const -> decltype(m_size) final;
     auto               setSize(const decltype(m_size) &size) -> void final;
+    [[nodiscard]] auto getBody() -> WindowBody &;
 
     auto mouseButtonEvent(const Abyss::GUI::InputDevice &device, Abyss::GUI::InputDefinitions::MOUSE_BUTTON button, Abyss::GUI::InputDefinitions::ACTION action) -> void final;
     auto think(const Abyss::GUI::InputDevice &device, double dt) -> void final;
@@ -46,8 +48,8 @@ public:
 
 inline auto SFMLWindow::getPosition(bool relative) const -> decltype(m_position)
 {
-	if (relative == true)
-		return {};
+    if (relative == true)
+        return {};
 
     auto pos{m_window->getPosition()};
     return {static_cast<double>(pos.x), static_cast<double>(pos.y)};
@@ -56,41 +58,54 @@ inline auto SFMLWindow::getPosition(bool relative) const -> decltype(m_position)
 inline auto SFMLWindow::setPosition(const decltype(m_position) &position) -> void
 {
     Widget::setPosition(position);
-	m_window->setPosition({static_cast<int>(position.x), static_cast<int>(position.y)});
+    m_window->setPosition({static_cast<int>(position.x), static_cast<int>(position.y)});
 }
 
 inline auto SFMLWindow::getSize() const -> decltype(m_size)
 {
-	auto size{m_window->getSize()};
+    auto size{m_window->getSize()};
     return {static_cast<double>(size.x), static_cast<double>(size.y)};
 }
 
 inline auto SFMLWindow::setSize(const decltype(m_size) &size) -> void
 {
     Widget::setSize(size);
-	m_window->setSize({static_cast<unsigned int>(size.x), static_cast<unsigned int>(size.y)});
+    m_window->setSize({static_cast<unsigned int>(size.x), static_cast<unsigned int>(size.y)});
+    m_body->setSize(size - Vector3<double>{0, 30});
+}
+
+inline auto SFMLWindow::getBody() -> WindowBody &
+{
+    return *m_body;
 }
 
 inline auto SFMLWindow::mouseButtonEvent(const Abyss::GUI::InputDevice &device, Abyss::GUI::InputDefinitions::MOUSE_BUTTON button, Abyss::GUI::InputDefinitions::ACTION action) -> void
 {
-    auto pos{getPosition()};
-    auto size{getSize()};
-    auto hovered{device.isHovering(pos, size)};
-	POINT point{};
-	GetCursorPos(&point);
-	Vector3<double> mouse{static_cast<double>(point.x), static_cast<double>(point.y)};
+    auto  pos{getPosition()};
+    auto  size{getSize()};
+    auto  hovered{device.isHovering(pos, {size.x, 30})};
+    POINT point{};
+    GetCursorPos(&point);
+    Vector3<double> mouse{static_cast<double>(point.x), static_cast<double>(point.y)};
 
     if (button == Abyss::GUI::InputDefinitions::MOUSE_BUTTON::MOUSE_BUTTON_LEFT && action == Abyss::GUI::InputDefinitions::ACTION::ACTION_PRESS && !m_held) {
         m_held = hovered;
         if (m_held)
             m_heldPosition = mouse - getPosition(false);
-		std::cout << "WOW\n";
     } else if (button == Abyss::GUI::InputDefinitions::MOUSE_BUTTON::MOUSE_BUTTON_LEFT && action == Abyss::GUI::InputDefinitions::ACTION::ACTION_RELEASE && m_held)
         m_held = false;
 }
 
 SFMLWindow::SFMLWindow()
 {
+    m_children.push_back(std::make_unique<WindowBody>());
+    m_body = static_cast<decltype(m_body)>(m_children.back().get());
+    if (m_body == nullptr)
+        throw std::runtime_error{"Couldn't create WindowBody"};
+
+    m_body->setPosition({0, 30});
+    getBody().addChild<Abyss::GUI::Widgets::Textbox>();
+
     m_window = std::make_unique<sf::RenderWindow>(sf::VideoMode{400, 400}, "AbyssGUI example", sf::Style::None);
     if (!m_window)
         throw std::runtime_error{"Couldn't create RenderWindow."};
@@ -103,7 +118,7 @@ SFMLWindow::SFMLWindow()
 
     registerOnInputDevice(input);
 
-/*
+    /*
     Abyss::GUI::Widgets::Window win{};
     auto &textbox{win.getBody().addChild<Abyss::GUI::Widgets::Textbox>()};
     auto &tabbar{win.getBody().addChild<TabBar>()};
@@ -134,12 +149,12 @@ SFMLWindow::SFMLWindow()
 
 inline auto SFMLWindow::think(const Abyss::GUI::InputDevice &device, double) -> void
 {
-	POINT point{};
-	GetCursorPos(&point);
-	Vector3<double> mouse{static_cast<double>(point.x), static_cast<double>(point.y)};
+    POINT point{};
+    GetCursorPos(&point);
+    Vector3<double> mouse{static_cast<double>(point.x), static_cast<double>(point.y)};
 
-	if (m_held)
-		setPosition(mouse - m_heldPosition);
+    if (m_held)
+        setPosition(mouse - m_heldPosition);
 }
 
 inline auto SFMLWindow::paint(const Abyss::GUI::InputDevice &device, double, Abyss::GUI::Renderer &renderer) -> void
@@ -150,6 +165,20 @@ inline auto SFMLWindow::paint(const Abyss::GUI::InputDevice &device, double, Aby
     renderer.setColor({0, 255, 0});
     renderer.drawRectangle(pos, size);
     renderer.drawText(L"Window Test", pos + Vector3<double>{6, 3}, "OpenSans-Regular.ttf", 18, {50, 50, 50});
+}
+
+auto main() -> int
+{
+    WindowsConsole test{};
+    try {
+        // start();
+        SFMLWindow win{};
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+        int tmp{};
+        std::cin >> tmp;
+    }
+    return 0;
 }
 
 // static auto start() -> void
@@ -182,16 +211,17 @@ inline auto SFMLWindow::paint(const Abyss::GUI::InputDevice &device, double, Aby
 //     }
 // }
 
-int main()
-{
-    // WindowsConsole test{};
-    try {
-        // start();
-        SFMLWindow();
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << '\n';
-        int tmp{};
-        std::cin >> tmp;
-    }
-    return 0;
-}
+// class TabBar : public Abyss::GUI::Widgets::RelativeLayout {
+// public:
+//     auto paint(const Abyss::GUI::InputDevice &device, double dt, Abyss::GUI::Renderer &renderer) -> void final;
+// };
+
+// inline auto TabBar::paint(const Abyss::GUI::InputDevice &device, double, Abyss::GUI::Renderer &renderer) -> void
+// {
+//     auto pos{getPosition()};
+//     auto size{getSize()};
+
+//     renderer.setColor({255, 255, 0});
+//     renderer.drawRectangle(pos, size);
+//     renderer.drawText(L"Tab 1\tTab 2\tTab 3", pos + Vector3<double>{10, 3}, "OpenSans-Regular.ttf", size.y - 3 * 3, {205, 205, 205});
+// }
